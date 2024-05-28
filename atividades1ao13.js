6f60b2f1f7f3cd17c7bbe4cf6fd1a2037a58e478
danisoaresl <daniele.soaresl@gmail.com>
2024-05-28 16:44:36 -0300

    atividades atualizadas 2805

--- atividades1ao13.js
+++ atividades1ao13.js
@@ -1,3 +1,31 @@
+//Desafios finais
+//Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+
+//Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+
+//Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+//Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+//Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+//Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+//Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+
+//Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+//Use um loop while para imprimir os números de 1 a 10 no console.
+
+//Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+//Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+//Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+
+//Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+
 // Exibir uma mensagem de boas-vindas no console
 console.log("Bem-vindo!");
 
---
+++ operador_ternario.js
@@ -0,0 +1,14 @@
+//Aline está dando seus primeiros passos na programação utilizando o JavaScript e recentemente descobriu uma funcionalidade que achou fascinante: o operador ternário.
+
+//Ela achou muito interessante a possibilidade que o operador traz para diminuir a quantidade de código escrito em algumas ocasiões e decidiu aplicar na prática, em um de seus projetos pessoais. No entanto, ao tentar fazer essa refatoração, acabou se esquecendo da sintaxe do operador ternário.
+
+//Sabendo que você estudou sobre o operador recentemente, ela pediu sua ajuda e mostrou o código que pretende refatorar:
+
+
+let palavraPessoa = "";
+
+if(quantidadePessoas == 1){
+    palavraPessoa = "pessoa";
+}else{
+    palavraPessoa = "pessoas"
+}
